7eb36d40341720bf6ccb3937a6665579fb9f005d
Miaoqi Chu axinite <mqichu@anl.gov>
2022-03-15 14:06:14 -0500

    allow config to overwrite default config

--- boost_corr/cli.py
+++ boost_corr/cli.py
@@ -33,6 +33,22 @@ def convert_to_list(input_str: str):
     return result
 
 
+default_config = {
+    "qmap": None,   # use a valid qmap fname here
+    "output": "cluster_results",
+    "smooth": "sqmap",
+    "gpu_id": -1,
+    "begin_frame": 1,
+    "end_frame": -1,
+    "stride_frame": 1,
+    "avg_frame": 1,
+    "type": "Multitau",
+    "dq_selection": "all",
+    "verbose": False,
+    "dryrun": False,
+    "overwrite": False
+}
+
 description = ("Compute Multi-tau/Twotime correlation for XPCS datasets on "
                "GPU/CPU")
 parser = argparse.ArgumentParser(description=description)
@@ -47,7 +63,8 @@ parser.add_argument("-r",
 parser.add_argument("-q",
                     "--qmap",
                     metavar="QMAP_FILENAME",
-                    required=True,
+                    default=default_config['qmap'],
+                    required=False,
                     type=str,
                     help="the filename of the qmap file (h5/hdf)")
 
@@ -56,7 +73,7 @@ parser.add_argument("-o",
                     metavar="OUTPUT_DIR",
                     type=str,
                     required=False,
-                    default="cluster_results",
+                    default=default_config['output'],
                     help="""[default: cluster_results] the output directory
                             for the result file. If not exit, the program will
                             create this directory.""")
@@ -66,7 +83,7 @@ parser.add_argument("-s",
                     metavar="SMOOTH",
                     type=str,
                     required=False,
-                    default="sqmap",
+                    default=default_config['smooth'],
                     help="""[default: sqmap] smooth method to be used in
                             Twotime correlation. """)
 
@@ -74,20 +91,20 @@ parser.add_argument("-i",
                     "--gpu_id",
                     metavar="GPU_ID",
                     type=int,
-                    default=-1,
+                    default=default_config['gpu_id'],
                     help="""[default: -1] choose which GPU to use. if the input
                             is -1, then CPU is used""")
 
 parser.add_argument("-begin_frame",
                     type=int,
-                    default=1,
+                    default=default_config['begin_frame'],
                     help="""[default: 1] begin_frame specifies which frame to
                             begin with for the correlation. This is useful to
                             get rid of the bad frames in the beginning.""")
 
 parser.add_argument("-end_frame",
                     type=int,
-                    default=-1,
+                    default=default_config['end_frame'],
                     help="""[default: -1] end_frame specifies the last frame
                             used for the correlation. This is useful to
                             get rid of the bad frames in the end. If -1 is
@@ -96,12 +113,12 @@ parser.add_argument("-end_frame",
 
 parser.add_argument("-stride_frame",
                     type=int,
-                    default=1,
+                    default=default_config['stride_frame'],
                     help="""[default: 1] stride_frame defines the stride.""")
 
 parser.add_argument("-avg_frame",
                     type=int,
-                    default=1,
+                    default=default_config['avg_frame'],
                     help="""[default: 1] stride_frame defines the number of
                             frames to be averaged before the correlation.""")
 
@@ -111,7 +128,7 @@ parser.add_argument("-t",
                     metavar="TYPE",
                     type=str,
                     required=False,
-                    default="Multitau",
+                    default=default_config['type'],
                     help="""
                         [default: "Multitau"] Analysis type: ["Multitau",
                         "Twotime", "Both"].""")
@@ -121,7 +138,7 @@ parser.add_argument("-dq",
                     metavar="TYPE",
                     type=str,
                     required=False,
-                    default="all",
+                    default=default_config['dq_selection'],
                     help="""
                         [default: "all"] dq_selection: a string that select
                         the dq list, eg. '1, 2, 5-7' selects [1,2,5,6,7].
@@ -129,19 +146,19 @@ parser.add_argument("-dq",
 
 parser.add_argument("--verbose",
                     "-v",
-                    default=False,
+                    default=default_config['verbose'],
                     action="store_true",
                     help="verbose")
 
 parser.add_argument("--dryrun",
                     "-dr",
-                    default=False,
+                    default=default_config['dryrun'],
                     action="store_true",
                     help="dryrun: only show the argument without execution.")
 
 parser.add_argument("--overwrite",
                     "-ow",
-                    default=False,
+                    default=default_config['overwrite'],
                     action="store_true",
                     help="whether to overwrite the existing result file.")
 
@@ -159,7 +176,6 @@ parser.add_argument("-c",
 
 args = parser.parse_args()
 kwargs = vars(args)
-kwargs['dq_selection'] = convert_to_list(kwargs['dq_selection'])
 
 if args.config is not None:
     config_fname = kwargs.pop("config")
@@ -168,9 +184,12 @@ if args.config is not None:
 
     common_keys = set(kwargs.keys()) & set(config.keys())
     for key in common_keys:
-        if kwargs[key] != config[key]:
+        if kwargs[key] != default_config[key]:
+            # only update the args that are different from the default ones
             del config[key]
     kwargs.update(config)
+
+kwargs['dq_selection'] = convert_to_list(kwargs['dq_selection'])
 
 # automatically obtain gpu id
 gpu_id_auto = None 
@@ -180,6 +199,7 @@ if kwargs['gpu_id'] == -2:
 
 def main():
     flag = 0
+
     if kwargs['dryrun']:
         ans = 'dryrun_only'
         print(json.dumps(kwargs, indent=4))
