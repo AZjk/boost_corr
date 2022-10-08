4411b3cb59117148ccfd6ef1f1e400842b868f34
mqichu <mqichu@anl.gov>
2022-10-07 22:22:49 -0500

    remove unused option

--- benchmark/benchmark_apsu.sh
+++ benchmark/benchmark_apsu.sh
@@ -4,13 +4,12 @@ QMAP=/data/xpcs8//partitionMapLibrary/2021-1/cssiTest202103_qmap_Aerogel_good_1.
 # for i in $(seq 7 12);
 #        --raw /data/xpcs8/APSU_TestData_202106/APSU_TestData_$idx/APSU_TestData_$idx.h5\
 #        --raw /clhome/MQICHU/ssd/APSU_TestData_202106/APSU_TestData_$idx/APSU_TestData_$idx.h5\
-for i in $(seq 8 10);
+for i in $(seq 1 3);
 do 
   idx=$(printf "%03d" $i)
   echo " "
   echo $idx
    boost_corr \
-       --batch_size 4 \
        --gpu_id 0    \
        --raw /clhome/MQICHU/ssd/APSU_TestData_202106/APSU_TestData_$idx/APSU_TestData_$idx.h5\
        --qmap $QMAP \
--- benchmark/benchmark_waxpcs.sh
+++ benchmark/benchmark_waxpcs.sh
@@ -1,7 +1,6 @@
 #  gpu_corr_shell \
 boost_corr \
-        --batch_size 1024 \
-        -i 2 \
+        -i 0 \
         -r /clhome/MQICHU/ssd/A050_Cu3Au_1bit_att0/A050_Cu3Au_1bit_att0.h5 \
         -q /net/s8iddata/export/8-id-e/partitionMapLibrary/2021-2/dufresne20210727_qmap_A050_1.h5 \
         -v \
