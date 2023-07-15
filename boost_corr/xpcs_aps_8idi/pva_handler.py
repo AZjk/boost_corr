58a29e01ab418c147d6dc738ef1438ea24f8bb59
Miaoqi Chu axinite <mqichu@anl.gov>
2023-07-15 14:17:42 -0500

    add pva handler

---
+++ boost_corr/xpcs_aps_8idi/pva_handler.py
@@ -0,0 +1,107 @@
+import numpy as np
+import torch
+import logging
+from .xpcs_dataset import XpcsDataset
+
+
+logger = logging.getLogger(__name__)
+
+
+class PvaDataset(XpcsDataset):
+    """
+    Parameters
+    ----------
+    filename: string
+    """
+    def __init__(self,
+                 *args,
+                 **kwargs):
+
+        super(PvaDataset, self).__init__(*args, **kwargs)
+        self.dataset_type = "PVA Dataset"
+        self.is_sparse = False
+
+    
+    def get_detector(self):
+            self.shape = data.shape
+            # update data type;
+            if data.dtype == np.uint8:
+                self.dtype = np.int16
+            elif data.dtype == np.uint16:
+                # lambda2m's uint16 is actually 12bit. it's safe to to int16
+                if self.shape[1] * self.shape[2] == 1813 * 1558:
+                    self.dtype = np.int16
+                # likely eiger detectors
+                else:
+                    self.dtype = np.int32
+            elif data.dtype == np.uint32:
+                self.dtype = np.int32
+                logger.warn('cast uint32 to int32. it may cause ' + 
+                            'overflow when the maximal value >= 2^31')
+            else:
+                self.dtype = data.dtype
+        
+        if kwargs['avg_frame'] > 1:
+            self.dtype = np.float32
+        self.fhdl = None
+        self.data = None
+        self.data_cache = None
+        self.data_path = data_path
+
+        self.update_batch_info(self.shape[0])
+        self.update_det_size(self.shape[1:])
+
+        if self.mask_crop is not None:
+            self.mask_crop = self.mask_crop.cpu().numpy()
+
+        self.current_group = None
+        self.preload_size = preload_size
+
+    def __reset__(self):
+        if self.fhdl is not None:
+            self.fhdl.close()
+            self.data = None
+            self.fhdl = None
+
+    def __getbatch__(self, idx):
+        """
+        return numpy array, which will be converted to tensor with dataloader
+        """
+        if self.fhdl is None:
+            self.fhdl = h5py.File(self.fname, 'r', rdcc_nbytes=1024*1024*256)
+            self.data = self.fhdl[self.data_path]
+
+        beg, end, size = self.get_raw_index(idx)
+        idx_list = np.arange(beg, end, self.stride)
+
+        if self.mask_crop is not None:
+            # x = self.data[beg:end, self.sl_v, self.sl_h].reshape(end - beg, -1)
+            x = self.data[idx_list].reshape(size, -1)
+            x = x[:, self.mask_crop].astype(self.dtype)
+            # if idx == 0:
+            #     print(np.max(x), x.dtype)
+        else:
+            x = self.data[idx_list].reshape(-1, self.pixel_num)
+
+        if x.dtype != self.dtype:
+            x = x.astype(self.dtype)
+        return torch.from_numpy(x)
+
+
+def test():
+    fname = (
+        "/clhome/MQICHU/ssd/xpcs_data_raw/A003_Cu3Au_att0_001/A003_Cu3Au_att0_001.imm"
+    )
+    ds = HdfDataset(fname)
+    # for n in range(len(ds)):
+    #     print(n, ds[n].shape, ds[n].device)
+
+
+def test_bin(idx):
+    fname = f"/clhome/MQICHU/ssd/APSU_TestData_202106/APSU_TestData_{idx:03d}/APSU_TestData_{idx:03d}.h5"
+    ds = HdfDataset(fname)
+    ds.to_rigaku_bin(f"hdf2bin_{idx:03d}.bin")
+
+
+if __name__ == '__main__':
+    test_bin(4)
