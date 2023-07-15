57261624da00d2bcf2c14026894d66cde1d8bf57
Miaoqi Chu axinite <mqichu@anl.gov>
2023-07-15 14:12:21 -0500

    add fname

--- boost_corr/xpcs_aps_8idi/xpcs_result.py
+++ boost_corr/xpcs_aps_8idi/xpcs_result.py
@@ -33,6 +33,7 @@ class XpcsResult(object):
                  meta_dir: str,
                  qmap_fname: str,
                  output_dir: str,
+                 fname=None,
                  overwrite=False,
                  avg_frame=1,
                  stride_frame=1,
@@ -41,7 +42,15 @@ class XpcsResult(object):
 
         meta_fname = get_metadata(meta_dir)
         # the filename for the result file;
-        fname = os.path.join(output_dir, os.path.basename(meta_fname))
+        if fname is None:
+            fname = os.path.join(output_dir, os.path.basename(meta_fname))
+        else:
+            # for pvapy applications
+            fname = fname.replace(':', '_')
+            if not fname.endswith('.hdf'):
+                fname += '.hdf'
+            fname = os.path.join(output_dir, fname)
+
         if analysis_type == 'Twotime':
             fname = fname.replace('.hdf', '_Twotime.hdf')
         # avoid overwrite
@@ -52,6 +61,7 @@ class XpcsResult(object):
             else:
                 fname = fname[:-7] + f'_{idx:02d}.hdf'
             idx += 1
+
         self.fname = fname
 
         try:
@@ -62,8 +72,11 @@ class XpcsResult(object):
             raise IOError(f'Check metadata file {meta_fname}')
         
     def save(self, result_dict, mode="alias", compression=None, **kwargs):
+        if 'saxs_2d_single' in result_dict:
+            result_dict.pop('saxs_2d_single')
         put(self.fname, result_dict, mode=mode, compression=compression,
             **kwargs)
+        logger.info(f'correlation result saved to [{self.fname}]')
 
 
 if __name__ == "__main__":
