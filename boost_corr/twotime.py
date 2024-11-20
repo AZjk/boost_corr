7c3b34655d24c9ee66d00b5297819fa29ba81e27
Miaoqi Chu <mqichu@anl.gov>
2024-11-20 01:03:39 -0600

    use multiple workers to load; simplify c2 calculation

--- boost_corr/twotime.py
+++ boost_corr/twotime.py
@@ -1,32 +1,40 @@
 import torch
+from tqdm import trange, tqdm
+from torch.utils.data import DataLoader
+import logging
 
 
-def create_diagonal_index(N=512):
-    m = torch.zeros((N, N), dtype=torch.int64)
-    x = torch.hstack([torch.arange(N - 1, 0, -1), torch.arange(N)])
+logger = logging.getLogger(__name__)
 
-    for n in range(N):
-        sl = slice(n, n + N)
-        m[n] = x[sl]
-    m = torch.flipud(m)
-    return m
+
+def create_diagonal_index(N=512, device='cpu'):
+    """
+    create a matrix of indices for diagonal averaging
+    :param N: size of the matrix
+    :return: NxN matrix with indices for diagonal averaging
+    Example:
+    >>> create_diagonal_index(5)
+    tensor([[0, 1, 2, 3, 4],
+            [1, 0, 1, 2, 3],
+            [2, 1, 0, 1, 2],
+            [3, 2, 1, 0, 1],
+            [4, 3, 2, 1, 0]])
+    """
+    indices = torch.arange(N, device=device)
+    diag_mat = torch.abs(indices.unsqueeze(0) - indices.unsqueeze(1))
+    return diag_mat
 
 
 def compute_diagonal_average(index, weights):
+    """
+    compute the diagonal average of a matrix using precomputed indices and weights
+    :param index:  matrix with indices for diagonal averaging
+    :param weights: NxN matrix with weights for diagonal averaging
+    """
     # index and weights must be both one d;
     tot = torch.bincount(index, weights=weights)
     pts = torch.bincount(index)
     return tot / pts
-
-
-# def convert_to():
-#     if mode != 'diag':
-#     c2v = np.zeros((c2.shape[0], c2.shape[1] * 2), np.float32)
-#     for n in range(c2.shape[0]):
-#         sl = slice(c2.shape[1] - n, c2.shape[1] * 2 - n)
-#         c2v[n, sl] = c2[n]
-#     c2v[c2v == 0] = np.min(c2)
-#     c2 = c2v
 
 
 class TwotimeCorrelator():
@@ -50,7 +58,6 @@ class TwotimeCorrelator():
         self.pixel_num = self.det_size[0] * self.det_size[1]
 
         self.device = device
-        self.cache = []
         self.frame_num = frame_num
         self.method = method
 
@@ -95,32 +102,61 @@ class TwotimeCorrelator():
             #     self.calc_normal_twotime()
         elif self.method == 'window':
             self.process_window(x)
+    
+    def process_dataset(self,
+                        ds,
+                        verbose=True,
+                        use_loader=False,
+                        num_workers=16):
+        if not use_loader:
+            xrange = trange if verbose else range
+            for n in xrange(len(ds)):
+                self.process(ds[n])
+        else:
+            # only pin memory if using GPU for computing
+            pin_memory = (self.device != 'cpu')
+            dl = DataLoader(ds,
+                            batch_size=None,
+                            pin_memory=pin_memory,
+                            num_workers=num_workers,
+                            prefetch_factor=4)
+            logger.info(f'using {num_workers} workers to load data')
+            
+            if verbose:
+                container = tqdm(dl, total=len(ds), desc='progress')
+            else:
+                container = dl
+
+            for x in container:
+                x = x.to(self.device, non_blocking=True)
+                self.process(x)
 
     def compute_average(self, num_part=10):
-        # saxs
-        self.pixel_sum_roi = torch.mean(self.cache, axis=0)
-        self.pixel_sum = torch.zeros(self.pixel_num,
-                                     dtype=torch.float32,
-                                     device=self.device)
-        self.pixel_sum[self.mask_crop] = self.pixel_sum_roi
-        self.pixel_sum = self.pixel_sum.reshape(*self.det_size)
-
         pixel_sum_par = torch.zeros(num_part,
                                     self.pixel_num,
                                     dtype=torch.float32,
                                     device=self.device)
+        self.pixel_sum = torch.zeros_like(pixel_sum_par[0])
 
         frame_len = self.frame_num // num_part
         for n in range(num_part):
-            sl = slice(n * frame_len, (n + 1) * frame_len)
-            avg = torch.mean(self.cache[sl], axis=0)
-            pixel_sum_par[n, self.mask_crop] = avg
+            if n == num_part - 1:
+                sl = slice(n * frame_len, self.frame_num)  # last part
+            else:
+                sl = slice(n * frame_len, (n + 1) * frame_len)
+            part_sum = torch.sum(self.cache[sl], axis=0)
+            self.pixel_sum[self.mask_crop] += part_sum
+            pixel_sum_par[n, self.mask_crop] = part_sum / (sl.stop - sl.start)
 
+        self.pixel_sum /= self.frame_num
+        self.pixel_sum = self.pixel_sum.reshape(*self.det_size)
         self.pixel_sum_par = pixel_sum_par.reshape(num_part, *self.det_size)
 
-        frame_sum = torch.mean(self.cache, axis=1)
-        taxis = torch.arange(self.frame_num, device=self.device)
-        self.frame_sum = torch.stack([taxis, frame_sum])
+        frame_sum_val = torch.mean(self.cache, axis=1)
+        # self.cache /= frame_sum_val.reshape(-1, 1)
+
+        frame_sum_xax = torch.arange(self.frame_num, device=self.device)
+        self.frame_sum = torch.stack([frame_sum_xax, frame_sum_val])
 
     def compute_smooth_data(self, mode='sqmap'):
         if mode == 'sqmap':
@@ -209,41 +245,37 @@ class TwotimeCorrelator():
         return saxs
 
     def calc_normal_twotime(self, num_partials=5):
-        diag_mat = create_diagonal_index(self.frame_num).to(self.device)
+        diag_mat = create_diagonal_index(self.frame_num, device=self.device)
         diag_mat_1d = diag_mat.reshape(-1)
         partial_len = self.frame_num // num_partials
-        diag_mat_1d_p = diag_mat[0:partial_len, 0:partial_len].reshape(-1)
+        diag_mat_1d_par = diag_mat[0:partial_len, 0:partial_len].reshape(-1)
 
         # triu_idx = torch.tril_indices(self.num_frames, self.num_frames)
-
-        for n, q in enumerate(self.dq_idx):
-            wf = self.cache[:, self.dq_slc[n]]
+        for n, _ in enumerate(self.dq_idx):
+            time_series = self.cache[:, self.dq_slc[n]]
             if self.sdata is not None:
-                wf = wf / self.sdata[self.dq_slc[n]]
+                time_series = time_series / self.sdata[self.dq_slc[n]]
 
-            aa = torch.sum(wf, dim=1)
-            aa[aa <= 0] = 1
-            aa_inverse = 1.0 / aa
+            norm_factor = torch.sum(time_series, dim=1)
+            norm_factor[norm_factor <= 0] = 1
+            norm_factor = 1.0 / norm_factor
 
-            ab = torch.matmul(wf, wf.T)
-            c2 = ab * aa_inverse
-            c2 = c2.T * aa_inverse * wf.shape[1]
+            matmul_prod = torch.matmul(time_series, time_series.T)
+            c2 = matmul_prod * norm_factor * norm_factor.reshape(-1, 1) * time_series.shape[1]
 
             g2full = compute_diagonal_average(diag_mat_1d, c2.reshape(-1))
             self.g2full.append(g2full)
 
             g2partial = []
-
             for idx in range(num_partials):
                 sl = slice(idx * partial_len, (idx + 1) * partial_len)
-                temp = compute_diagonal_average(diag_mat_1d_p,
+                temp = compute_diagonal_average(diag_mat_1d_par,
                                                 c2[sl, sl].reshape(-1))
                 g2partial.append(temp)
             g2partial = torch.stack(g2partial)
             self.g2partial.append(g2partial)
 
             yield (torch.triu(c2)).cpu().numpy()
-            # yield c2[triu_idx[0], triu_idx[1]].cpu().numpy()
 
 
 if __name__ == '__main__':
