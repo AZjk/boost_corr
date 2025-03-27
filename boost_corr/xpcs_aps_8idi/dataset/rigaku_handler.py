70ed7fd08a5de30f78bbbfee27eb14aaf91161e5
Miaoqi Chu <mqichu@anl.gov>
2025-03-27 16:50:11 -0500

    offload large files to CPU

--- boost_corr/xpcs_aps_8idi/dataset/rigaku_handler.py
+++ boost_corr/xpcs_aps_8idi/dataset/rigaku_handler.py
@@ -3,6 +3,7 @@ import logging
 import torch
 from .xpcs_dataset import XpcsDataset
 from .help_functions import convert_sparse
+import os
 
 
 logger = logging.getLogger(__name__)
@@ -15,6 +16,7 @@ class RigakuDataset(XpcsDataset):
     filename: string
         path to .imm file
     """
+
     def __init__(self, *args, dtype=np.uint8, total_frames=None, **kwargs):
         super(RigakuDataset, self).__init__(*args, dtype=dtype, **kwargs)
         self.dataset_type = "Rigaku 64bit Binary"
@@ -23,14 +25,21 @@ class RigakuDataset(XpcsDataset):
         self.ifc, self.mem_addr = self.read_data(total_frames)
         self.ifc = self.to_device()
 
-    def to_device(self):
-        index = torch.tensor(self.ifc[0].astype(np.int32), device=self.device)
-        frame = torch.tensor(self.ifc[1].astype(np.int32), device=self.device)
-        count = torch.tensor(self.ifc[2].astype(np.uint8), device=self.device)
+    def to_device(self, max_fsize_gb_in_gpu=1.0):
+        fsize = os.path.getsize(self.fname) / 1024**3
+        if fsize <= max_fsize_gb_in_gpu and self.device.startswith("cuda"):
+            device = self.device
+        else:
+            device = "cpu"
+            logger.info(f"{self.fname} is {fsize:.2f} GB, offloading to CPU RAM")
+
+        index = torch.tensor(self.ifc[0].astype(np.int32), device=device)
+        frame = torch.tensor(self.ifc[1].astype(np.int32), device=device)
+        count = torch.tensor(self.ifc[2].astype(np.uint8), device=device)
         return (index, frame, count)
 
     def read_data(self, total_frames=None):
-        with open(self.fname, 'r') as f:
+        with open(self.fname, "r") as f:
             a = np.fromfile(f, dtype=np.uint64)
             d = convert_sparse(a)
             index, frame, count = d[0], d[1], d[2]
@@ -57,32 +66,33 @@ class RigakuDataset(XpcsDataset):
     def __getbatch__(self, idx):
         # frame begin and end
         beg, end, size = self.get_raw_index(idx)
-        x = torch.zeros(size, self.pixel_num, dtype=torch.uint8,
-                        device=self.device)
+        device = self.ifc[0].device
+        x = torch.zeros(size, self.pixel_num, dtype=torch.uint8, device=device)
         if self.stride == 1:
             # the data is continuous in RAM; convert by batch
             sla, slb = self.mem_addr[beg], self.mem_addr[end]
             a, b = sla.start, slb.start
-            x[self.ifc[1][a:b].long() - beg, self.ifc[0][a:b].long()] = \
-                self.ifc[2][a:b]
+            x[self.ifc[1][a:b].long() - beg, self.ifc[0][a:b].long()] = self.ifc[2][a:b]
         else:
             # the data is discrete in RAM; convert frame by frame
             for n, idx in enumerate(np.arange(beg, end, self.stride)):
                 sl = self.mem_addr[idx]
                 x[n, self.ifc[0][sl].long()] = self.ifc[2][sl]
-        
+        x = x.to(self.device)
         if self.mask_crop is not None:
             x = x[:, self.mask_crop]
         return x
 
 
 def test():
-    fname = ("/home/beams/MQICHU/ramdisk/O018_Silica_D100_att0_Rq0_00001/"
-             "O018_Silica_D100_att0_Rq0_00001.bin")
+    fname = (
+        "/home/beams/MQICHU/ramdisk/O018_Silica_D100_att0_Rq0_00001/"
+        "O018_Silica_D100_att0_Rq0_00001.bin"
+    )
     ds = RigakuDataset(fname)
     # for n in range(len(ds)):
     #     print(n, ds[n].shape)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test()
