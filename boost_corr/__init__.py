47dc9b1090fdefea01d770e4c03f56a9db9c9775
Miaoqi Chu <mqichu@anl.gov>
2025-11-17 14:36:35 -0600

    refactor: migrate from pkg_resources to importlib.metadata

--- boost_corr/__init__.py
+++ boost_corr/__init__.py
@@ -1,20 +1,20 @@
 """Top-level package for boost-corr."""
 
 __author__ = """Miaoqi Chu"""
-__email__ = 'mqichu@anl.gov'
+__email__ = "mqichu@anl.gov"
 
+from importlib.metadata import version, PackageNotFoundError
 
-from pkg_resources import get_distribution, DistributionNotFound
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
+    __version__ = version(__name__)
+except PackageNotFoundError:
     __version__ = "0.0.0"
 
-
 import logging
-logging.getLogger('boost_corr').addHandler(logging.NullHandler())
 
+logging.getLogger("boost_corr").addHandler(logging.NullHandler())
 
 from .multitau import MultitauCorrelator
 from .twotime import TwotimeCorrelator
+
 __all__ = (MultitauCorrelator, TwotimeCorrelator)
